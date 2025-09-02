612d3e3fdb992ca6969c30df85553006eb51ecf6
Nikhil Raj <rajnikhil4795@gmail.com>
2025-09-03 01:51:50 +0530

    first logic

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from typing-speed-tester!")
-
-
-if __name__ == "__main__":
-    main()
+import time
+text = '''The entire competition is conducted online. All learning materials and resources tailored to your chosen theme will be provided. Teams will complete tasks and submit their work through the competition portal. Interactive online sessions and a dedicated discussion forum will help you resolve queries and connect with mentors throughout the event.'''
+words = text.split()
+print(text)
+check = input("Press 'S' to start : ")
+if check.lower() == 's':
+    start =time.time()
+test = input("Start Typing\n")
+end = time.time()
+noOfWords = len(words)
+timetaken = (end-start)/60
+noOfWordsTyped = len(test.split())
+wpm = noOfWordsTyped/timetaken
+print(f"Typing Speed is : {wpm:.2f}")
